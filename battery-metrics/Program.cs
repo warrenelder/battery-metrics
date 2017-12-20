906013496e64f2c135ad59de5e6531f8ff36db77
Warren Elder <warrenelder84@gmail.com>
2017-12-21 11:01:08 +1300

    Initial commit

---
+++ battery-metrics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace battery_metrics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
