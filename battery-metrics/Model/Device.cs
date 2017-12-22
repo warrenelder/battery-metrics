42baefe66033e76b3f06c9e4788977fe2e209588
Warren Elder <warrenelder84@gmail.com>
2017-12-23 11:14:15 +1300

    Define device model

---
+++ battery-metrics/Model/Device.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace batterymetrics.Model
+{
+    public class Device
+    {
+        public int deviceId { get; set; }
+        public int accntNo { get; set; }
+        public DateTime timestamp { get; set; } 
+        public string jsonData { get; set; }
+    }
+}
