91fe8ce06b70a98d3595fd85076f4336bb57fec0
Warren Elder <warrenelder84@gmail.com>
2017-12-23 15:21:20 +1300

    Refactor controller and set device array as a list

--- battery-metrics/Controller/ConsoleController.cs
+++ battery-metrics/Controller/ConsoleController.cs
@@ -1,17 +1,57 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using batterymetrics.Utilities;
+using static Newtonsoft.Json.JsonConvert;
+using batterymetrics.Model;
+using System.Collections.Generic;
 
 namespace batterymetrics.Controller
 {
     public class ConsoleController
     {
+        private static List<Device> _devices = new List<Device>();
+        private static List<Metric> _metrics = new List<Metric>();
+        private static List<Cycle> _cycles = new List<Cycle>();
+
         public void Read(string path)
         {
-            var devices = File.ReadAllLines(path).Skip(1)
-                                 .Select(l => DeviceParser.FromTsv(l))
-                                 .GroupBy(d => d.deviceId)
-                                 .ToArray();
+            _devices.AddRange(File.ReadAllLines(path).Skip(1).Select(l => DeviceParser.FromTsv(l)));
         }
+
+        public void Analyse()
+        {
+            var groupedDevices = _devices.GroupBy(d => d.deviceId).ToList();
+            foreach (var _device in groupedDevices)
+            {
+                int count = 0;
+                var deviceId = _device.Key;
+                Console.WriteLine(deviceId + " device id");
+                var test = _device.OrderBy(t => t.timestamp);
+                foreach(var _reading in _device) 
+                {
+                    count++;
+                    var deviceData = DeserializeObject<DeviceData>(_reading.jsonData);
+
+                    Console.WriteLine(_reading.timestamp);
+                    Console.WriteLine(deviceData.Battery.charging);
+                    Console.WriteLine(deviceData.Battery.level);
+
+                }
+
+                // Metric calculation
+            }
+        }
+
+        public void View()
+        {
+            
+        }
+
+        public void Create()
+        {
+            
+        }
+
     }
 }
--- battery-metrics/Model/Analysis.cs
+++ battery-metrics/Model/Analysis.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 namespace batterymetrics.Model
 {
-    public class Battery
+    public class Metric
     {
-        public Battery()
-        {
-        }
+        public int DeviceId { get; set; }
+        public int AccNum { get; set; }
+        public int BatteryLifetime { get; set; }
+        public int Cycles { get; set; }
+    }
+
+    public class Cycle
+    {
+        public int DeviceId { get; set; }
+        public enum Type { C, D, L }
+        public int ChargeStart { get; set; }
+        public int ChargeEnd { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }
