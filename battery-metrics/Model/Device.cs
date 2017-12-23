74b6535cb43e863e46e6d42d8616135e6c949fbf
Warren Elder <warrenelder84@gmail.com>
2017-12-23 14:21:51 +1300

    Define jsondata types

--- battery-metrics/Model/Device.cs
+++ battery-metrics/Model/Device.cs
@@ -8,4 +8,58 @@ namespace batterymetrics.Model
         public DateTime timestamp { get; set; } 
         public string jsonData { get; set; }
     }
+
+    public class DeviceData
+    {
+        public Wifi Wifi { get; set; }
+        public Battery Battery { get; set; }
+        public Cellular Cellular { get; set; }
+        public CurrentLocation CurrentLocation { get; set; }
+    }
+
+    public class Battery
+    {
+        public int level { get; set; }
+        public bool charging { get; set; }
+    }
+
+    public class CurrentLocation
+    {
+        public string uid { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Altitude { get; set; }
+        public double Accuracy { get; set; }
+        public int Timestamp { get; set; }
+        public int RequestTime { get; set; }
+        public int TimeToFix { get; set; }
+        public bool IsValid { get; set; }
+        public string AsText { get; set; }
+    }
+
+    public class Cellular
+    {
+        public int cellId { get; set; }
+        public string ipAddress { get; set; }
+        public int mcc { get; set; }
+        public string apn { get; set; }
+        public int lac { get; set; }
+        public string status { get; set; }
+        public bool isRoaming { get; set; }
+        public int mnc { get; set; }
+        public string imei { get; set; }
+        public string ipv6Address { get; set; }
+        public bool isFlightMode { get; set; }
+    }
+
+    public class Wifi
+    {
+        public string status { get; set; }
+        public string ipv6Address { get; set; }
+        public string ipAddress { get; set; }
+        public int signalStrength { get; set; }
+        public string ssid { get; set; }
+        public string macAddress{ get; set; }
+    }
+
 }
